52f7347e1fe3a30d5ae46e199dc81ce9b0e6bc4d
julian <julianhe921@gmail.com>
2024-04-27 09:50:47 +0200

    Add some funktions

--- figurify/plane_metrics/circle.py
+++ figurify/plane_metrics/circle.py
@@ -14,3 +14,14 @@ def radius(area):
 	value = (area / 3.14159) ** 0.5
 	return value
 
+def radius_from_circumference(circumference):
+	value = circumference / (2 * 3.14159)
+	return value
+
+def area_from_circumference(circumference):
+	value = (circumference**2) / (4 * 3.14159)
+	return value
+
+def circumference_from_diameter(diameter):
+	value = 3.14159 * diameter
+	return value
