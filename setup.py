cefc53c410a547504bc2b257b8716c0a9e89e4be
julian <julianhe921@gmail.com>
2024-04-28 16:18:42 +0200

    Update setup.py and add a README.md

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
+import pathlib
 
 setup(
     name='figurify',
     version='0.1',
     author='Julian Hess',
     description='With figurify you can calculate figures, surfaces and physics.',
-    long_description='Figurify is a Python library that allows you to perform various calculations related to geometric figures.'
-                     ' With Figurify, you can quickly and easily calculate properties such as'
-                     ' perimeter, area, angles, and more for various geometric shapes.',
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url='https://github.com/julian-hess/Figurify.git',
     packages=find_packages(),
+    project_urls={
+        "Source": "https://github.com/julian-hess/Figurify.git"
+    },
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
     ],
 )
